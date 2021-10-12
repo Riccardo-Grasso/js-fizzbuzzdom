9acb9377f0ba38f40016b4fac768ef2530052ebc
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-12 17:31:50 +0200

    versione_1

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,44 @@
 const tabContainer = document.getElementById("tab_container");
+tabContainer.classList.add("fw-bold");
 
 const row = document.createElement("div");
-row.classList.add("row", "row-cols-7", "gx-2");
+row.classList.add("row", "g-5");
 tabContainer.append(row);
 
 for (let i = 1; i <= 100; i++) {
 
-    let bgColor = "";
+    let bgColor = "bg-primary";
+    let contenuto = i;
 
     const multiplo3 = i % 3 === 0;
+    const multiplo5 = i % 5 === 0;
 
-    if (multiplo3) {
+
+    if (multiplo3 && multiplo5) {
+        bgColor = "bg-danger";
+        contenuto = "fizzbuzz";
+    } else if (multiplo5) {
+        bgColor = "bg-warning";
+        contenuto = "buzz";
+    } else if (multiplo3) {
         bgColor = "bg-success";
+        contenuto = "fizz";
     }
 
 
-    const col = document.createElement("div");
-    col.classList.add("col", "border", "ratio", "ratio-1x1", "d-flex", "justify-content-center", "align-items-center", `"${bgColor}"`);
+    const myCol = document.createElement("div");
+    myCol.classList.add("my-col", "d-flex", "justify-content-center", "align-items-center");
 
-    col.textContent = `${i}`;
 
-    row.append(col);
+
+
+    const myBox = document.createElement("div");
+    myBox.classList.add("my-box", "ratio", "ratio-1x1", "d-flex", "justify-content-center", "align-items-center", bgColor);
+    myBox.textContent = contenuto;
+
+
+    row.append(myCol);
+    myCol.append(myBox);
 }
 
 
